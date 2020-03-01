d7708299e19d20ad8e8cf76f0b8d6aaad61e651e
rishabhp2019 <55821392+rishabhp2019@users.noreply.github.com>
2020-02-29 22:05:21 -0500

    Update shopping.js

--- shopping.js
+++ shopping.js
@@ -1,16 +1,47 @@
-/**
-Creates a shopping list and displays it on shopping.innerHTML
-*/
+window.onload = function(){
 
-//
-window.onload= function(){
-//You will call the functions you wrote here
+	printList();
+
+	var name = prompt("Name of Product");
+    var price = prompt("Price of Product");
+    var amount = prompt("Amount of Product");
+    if (name != null){
+        list.name += name;
+    }
+    if (price != null){
+        list.price += price;
+    }
+    if (amount != null){
+        list.count += amount;
+    } 
 }
 
-
-//Prints the shopping list on the web page by changing the html code
-//list: Array of items
 function printList(list){
+	let name = prompt("What would you like to buy?");
+	let price = prompt("What is the price?");
+	let count = prompt("How many do you want?");
+	if(price != null && count !=null){
+		total += parseInt(price)*parseInt(count);
+	}
+	list = [{name:name, price:price, count:count}];
+	while(true){
+		if (name === null || price == null || count == null){
+			console.log(total);
+			list.pop()
+			break;
+		}
+		else{
+			name = prompt("What would you like to buy?");
+			price = prompt("What is it's price?");
+			count = prompt("How many do you want?");
+			if(price != null && count != null){
+				total += parseInt(price)*parseInt(count);
+			}
+			list.push({name:name, price:price, count:count})
+			console.log(total);
+		}
+
+}
     for(let i = 0; i < list.length; i++){
         document.getElementById("shoppingList").innerHTML += "<tr><td>" + list[i].name  + "</td> <td>" + list[i].price  + "</td> <td>" + list[i].count  +'</td></tr>';
     }
